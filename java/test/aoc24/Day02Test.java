76ac6b7547910a25a08f9ecbbcb75561e0764ddb
Duke <duke@home.com>
2024-12-20 17:50:35 -0500

    fix test

--- java/test/aoc24/Day02Test.java
+++ java/test/aoc24/Day02Test.java
@@ -13,6 +13,7 @@ public class Day02Test {
     void testCountSafeReports() throws IOException {
         Path inputPath = Files.createTempFile("test", ".txt");
         Files.writeString(inputPath, "7 6 4 2 1\n1 2 7 8 9\n9 7 6 2 1\n1 3 2 4 5\n8 6 4 4 1\n1 3 6 7 9");
-        assertEquals(2, Day02.countSafeReports(inputPath));
+        assertEquals(2, Day02.countSafeReports(inputPath, false));
+        assertEquals(4, Day02.countSafeReports(inputPath, true));
     }
 }
