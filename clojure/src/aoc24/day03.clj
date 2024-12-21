f0bdb542c44f0cef0ab536987a0608ffa37e0526
Duke <duke@home.com>
2024-12-20 22:28:40 -0500

    feat: Add solution for day 3, part 1 and 2

---
+++ clojure/src/aoc24/day03.clj
@@ -0,0 +1,14 @@
+(ns aoc24.day03
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(def input (->> (slurp (io/resource "aoc24/day03.txt"))
+                (str/split-lines)))
+
+(defn part-1
+  "Run with bb -x aoc24.day03/part-1"
+  [_])
+
+(defn part-2
+  "Run with bb -x aoc24.day02/part-2"
+  [_])
