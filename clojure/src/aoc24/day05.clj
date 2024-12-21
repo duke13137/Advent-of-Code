44d6dda5b445a25fa904953e4fd5f7c36053af42
Duke <duke@home.com>
2024-12-21 18:52:38 -0500

    refactor: Simplify valid placement check in update-sorted

--- clojure/src/aoc24/day05.clj
+++ clojure/src/aoc24/day05.clj
@@ -55,9 +55,9 @@
                                        (.indexOf (concat sorted-update [val]) y))
                                     true))
                                 (applicable-rules (concat sorted-update [val]) rules)))]
-        (if (every? #(valid-placement? next-val) (range (inc (count sorted-update))))
+        (if (every? valid-placement?  (range (inc (count sorted-update))))
           (recur (conj sorted-update next-val) (rest remaining-update))
-          (let [correct-index (first (filter #(valid-placement? next-val) (range (inc (count sorted-update)))))]
+          (let [correct-index (first (filter valid-placement? (range (inc (count sorted-update)))))]
             (recur (-> (subvec sorted-update 0 correct-index)
                        (conj next-val)
                        (into (subvec sorted-update correct-index)))
