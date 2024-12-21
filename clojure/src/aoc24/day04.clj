71c30da1fb03e207c63a077f20886a579d0a0ab4
Duke <duke@home.com>
2024-12-21 10:52:24 -0500

    feat: optimize day04 solution
    Refactor the solution to use a more efficient approach for counting occurrences and identifying XMAS shapes.
    - Introduce `extract-string` to simplify string extraction in different directions.
    - Optimize `count-occurrences` to use `reduce` for iterating over rows, columns, and directions.
    - Refactor `is-xmas-shape` to use `check-diagonal-pair` for diagonal checks.
    - Remove unnecessary `io` dependency.
    - Rename `part-1` and `part-2` to `part1` and `part2`.

--- clojure/src/aoc24/day04.clj
+++ clojure/src/aoc24/day04.clj
@@ -1,93 +1,66 @@
 (ns aoc24.day04
-  (:require [clojure.java.io :as io]
-            [clojure.string :as str]))
+  (:require [clojure.string :as str]))
 
-(def input (->> (slurp (io/resource "aoc24/day04.txt"))
-                (str/split-lines)))
+(defn parse-grid [input]
+  (str/split-lines input))
 
-(defn count-occurrences [grid]
-  (let [rows (count grid)
-        cols (count (first grid))
-        word "XMAS"
-        word-len (count word)]
-    (loop [row 0
-           col 0
-           count 0]
-      (if (>= row rows)
-        count
-        (if (>= col cols)
-          (recur (inc row) 0 count)
-          (let [
-                horizontal (if (<= (+ col word-len) cols) (subs (nth grid row) col (+ col word-len)) "")
-                vertical (if (<= (+ row word-len) rows) (apply str (map #(nth % col) (subvec grid row (+ row word-len)))) "")
-                diag-down-right (if (and (<= (+ row word-len) rows) (<= (+ col word-len) cols))
-                                  (apply str (map #(nth (nth grid (+ row %)) (+ col %)) (range word-len)))
-                                  "")
-                diag-down-left (if (and (<= (+ row word-len) rows) (>= (- col word-len -1) 0))
-                                 (apply str (map #(nth (nth grid (+ row %)) (- col %)) (range word-len)))
-                                 "")
-                
-                count (+ count
-                         (if (= horizontal word) 1 0)
-                         (if (= (str/reverse horizontal) word) 1 0)
-                         (if (= vertical word) 1 0)
-                         (if (= (str/reverse vertical) word) 1 0)
-                         (if (= diag-down-right word) 1 0)
-                         (if (= (str/reverse diag-down-right) word) 1 0)
-                         (if (= diag-down-left word) 1 0)
-                         (if (= (str/reverse diag-down-left) word) 1 0))]
-            (recur row (inc col) count)))))))
-
-(defn is-xmas-shape [grid row col]
+(defn- extract-string [grid row col word-len dir-row dir-col]
   (let [rows (count grid)
         cols (count (first grid))]
-    (if (or (< row 1) (>= row (dec rows)) (< col 1) (>= col (dec cols)))
-      false
-      (let [center (get-in grid [row col])
-            word "MAS"
-            reversed-word (str/reverse word)]
-        (if (not= center \A)
-          false
-          (let [
-                top (get-in grid [(dec row) col])
-                bottom (get-in grid [(inc row) col])
-                left (get-in grid [row (dec col)])
-                right (get-in grid [row (inc col)])
-                top-left (get-in grid [(dec row) (dec col)])
-                top-right (get-in grid [(dec row) (inc col)])
-                bottom-left (get-in grid [(inc row) (dec col)])
-                bottom-right (get-in grid [(inc row) (inc col)])
-                ]
-            (cond
-              (and (= top \M) (= bottom \M) (= left \S) (= right \S)) true
-              (and (= top \S) (= bottom \S) (= left \M) (= right \M)) true
-              (and (= top \M) (= bottom \M) (= left \S) (= right \S)) true
-              (and (= top \S) (= bottom \S) (= left \M) (= right \M)) true
-              (and (= top-left \M) (= bottom-right \M) (= top-right \S) (= bottom-left \S)) true
-              (and (= top-left \S) (= bottom-right \S) (= top-right \M) (= bottom-left \M)) true
-              (and (= top-left \M) (= bottom-right \M) (= top-right \S) (= bottom-left \S)) true
-              (and (= top-left \S) (= bottom-right \S) (= top-right \M) (= bottom-left \M)) true
-              :else false
-              )))))))
+    (loop [i 0
+           s ""]
+      (if (>= i word-len)
+        s
+        (let [new-row (+ row (* i dir-row))
+              new-col (+ col (* i dir-col))]
+          (if (and (>= new-row 0) (< new-row rows) (>= new-col 0) (< new-col cols))
+            (recur (inc i) (str s (get-in grid [new-row new-col])))
+            ""))))))
+
+(defn count-occurrences [grid word]
+  (let [rows (count grid)
+        cols (count (first grid))
+        word-len (count word)
+        directions [[0 1] [1 0] [1 1] [1 -1]]]
+    (reduce
+     (fn [count row]
+       (+ count (reduce
+                 (fn [col-count col]
+                   (+ col-count (reduce
+                                 (fn [dir-count [dir-row dir-col]]
+                                   (let [forward (extract-string grid row col word-len dir-row dir-col)
+                                         backward (str/reverse forward)]
+                                     (+ dir-count (if (= forward word) 1 0) (if (= backward word) 1 0))))
+                                 0 directions)))
+                 0 (range cols))))
+     0 (range rows))))
+
+(defn- check-diagonal-pair [grid row col up-dir-row up-dir-col down-dir-row down-dir-col word reversed-word]
+  (let [up-diag (extract-string grid (+ row up-dir-row) (+ col up-dir-col) (count word) up-dir-row up-dir-col)
+        down-diag (extract-string grid (+ row down-dir-row) (+ col down-dir-col) (count word) down-dir-row down-dir-col)]
+    (and (or (= up-diag word) (= up-diag reversed-word))
+         (or (= down-diag word) (= down-diag reversed-word)))))
+
+(defn- is-xmas-shape [grid row col]
+  (let [word "MAS"
+        reversed-word (str/reverse word)]
+    (and (= \A (get-in grid [row col]))
+         (check-diagonal-pair grid row col -1 -1 1 1 word reversed-word)
+         (check-diagonal-pair grid row col -1 1 1 -1 word reversed-word))))
 
 (defn count-xmas-shapes [grid]
   (let [rows (count grid)
         cols (count (first grid))]
-    (loop [row 0
-           col 0
-           count 0]
-      (if (>= row rows)
-        count
-        (if (>= col cols)
-          (recur (inc row) 0 count)
-          (recur row (inc col) (if (is-xmas-shape grid row col) (inc count) count)))))))
+    (reduce
+     (fn [count row]
+       (+ count (reduce
+                 (fn [col-count col]
+                   (+ col-count (if (is-xmas-shape grid row col) 1 0)))
+                 0 (range 1 (- cols 1)))))
+     0 (range 1 (- rows 1)))))
 
-(defn part-1
-  "Run with bb -x aoc24.day04/part-1"
-  [_]
-  (prn (count-occurrences input)))
+(defn part1 [input]
+  (count-occurrences (parse-grid input) "XMAS"))
 
-(defn part-2
-  "Run with bb -x aoc24.day04/part-2"
-  [_]
-  (prn (count-xmas-shapes input)))
+(defn part2 [input]
+  (count-xmas-shapes (parse-grid input)))
