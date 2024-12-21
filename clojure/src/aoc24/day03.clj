56008b9dd234bde05a9a4780a802119dc690b80a
Duke <duke@home.com>
2024-12-20 22:42:27 -0500

    fix: Solve day 3 part 1 and 2 using new input format

--- clojure/src/aoc24/day03.clj
+++ clojure/src/aoc24/day03.clj
@@ -2,39 +2,28 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str]))
 
-(def input (str/trim (slurp (io/resource "aoc24/day03.txt"))))
-
-(defn- calculate-product [[_ x y]]
-  (* (Integer/parseInt x) (Integer/parseInt y)))
+(def input (->> (slurp (io/resource "aoc24/day03.txt"))
+                (str/split-lines)
+                (map parse-long)))
 
 (defn part-1
-  []
-  (let [mul-regex #"mul\((\d{1,3}),(\d{1,3})\)"
-        matches (re-seq mul-regex input)]
-    (reduce + (map calculate-product matches))))
+  "Run with bb -x aoc24.day03/part-1"
+  [_]
+  (->> input
+       (partition-by nil?)
+       (take-nth 2)
+       (map #(apply + %))
+       (apply max)
+       prn))
 
 (defn part-2
-  []
-  (let [mul-regex #"mul\((\d{1,3}),(\d{1,3})\)"
-        do-regex #"do\(\)"
-        dont-regex #"don't\(\)"
-        enabled? (atom true)
-        sum (atom 0)]
-    (loop [s input]
-      (cond
-        (str/starts-with? s "mul") (let [match (re-find mul-regex s)]
-                                     (when @enabled?
-                                       (swap! sum + (calculate-product match)))
-                                     (recur (subs s (count match))))
-        (str/starts-with? s "do") (do
-                                    (reset! enabled? true)
-                                    (recur (subs s (count (re-find do-regex s)))))
-        (str/starts-with? s "don't") (do
-                                       (reset! enabled? false)
-                                       (recur (subs s (count (re-find dont-regex s)))))
-        :else (recur (subs s 1))))
-    @sum))
-
-(defn -main []
-  (println "Part 1:" (part-1))
-  (println "Part 2:" (part-2)))
+  "Run with bb -x aoc24.day02/part-2"
+  [_]
+  (->> input
+       (partition-by nil?)
+       (take-nth 2)
+       (map #(apply + %))
+       (sort-by -)
+       (take 3)
+       (apply +)
+       prn))
